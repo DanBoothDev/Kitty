113da006166be9818811922a26c181c975e67383
Dan Booth <DanBoothDev@gmail.com>
2020-12-06 11:59:33 -0400

    Various code style cleanup

--- kitty.c
+++ kitty.c
@@ -1,4 +1,5 @@
 #include "kitty.h"
+
 #include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -6,21 +7,25 @@
 #include <string.h>
 #include <unistd.h>
 
-FILE *inputFile;  // file to store the filename in
+#define asciiMinus 45
+#define asciiControlCode 33
+#define asciiSymbolEnd 64
+
+FILE *inputFile;
 int input;
-int nflag = 0;    // for -n
-int Eflag = 0;    // for -E
-int eflag = 0;    // for -E
-int bflag = 0;    // for -b
-int sflag = 0;    // for -s
-int vflag = 0;    // for -v
-int tflag = 0;    // for -t
+int nflag = 0;    // for -n - display line numbers
+int Eflag = 0;    // for -E - display $ at the end of each line
+int bflag = 0;    // for -b - number non-blank output lines
+int sflag = 0;    // for -s - squeeze multiple adjacent empty lines into one
+int vflag = 0;    // for -v - visibly display non-printing char
+int tflag = 0;    // for -t - equivalent to -v, also displaying tab char as ^I
+int eflag = 0;    // for -e - equivalent to -vE
 int fileIdx = 1;  // position within the argv array where the file exists
 
 int main(int argc, char *argv[]) {
   // char to read in temporarily
   int c = 0;
-  char option[] = {"benstuvE"};
+  char option[] = {"benstuvEh"};
   // scan single option commands in
   while ((c = getopt(argc, argv, option)) != -1) {
     switch (c) {
@@ -45,9 +50,8 @@ int main(int argc, char *argv[]) {
       case 't':
         tflag = 1;
         break;
-      case '?':
-        fprintf(stderr, "usage: cat [-%s] [file ...]\n",
-                option);  // error handling for unknown parameter passed
+      case 'h':
+        fprintf(stderr, "usage: cat [-%s] [file ...]\n", option);
         return 0;
       default:
         break;
@@ -55,27 +59,19 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  int minus = 45;  // ascii value for - (arguement)
-
-  // find where the file begins by checking if it begins with - signifying an
-  // arguement
+  // check where the filename begins
   for (int i = 1; i < argc; i++) {
-    if (argv[i][0] == minus) {
-      fileIdx++;
-    } else {
+    if (argv[i][0] != asciiMinus) {
       break;
     }
+    fileIdx++;
   }
-  // testing arguements for method type to trigger
-  if (argc == 1) {
-    // just plain Kitty
-    readFile(NULL);
-  } else if (argv[fileIdx] == NULL) {
+
+  if (argc == 1 || argv[fileIdx] == NULL) {
     // if no file specified stdin is then suggested
     readFile(NULL);
   } else {
-    // begin function with supplied filename which is located at fileIdx in the
-    // argv array
+    // read multiple files with supplied filename
     for (int i = fileIdx; i < argc; i++) {
       readFile(argv[i]);
     }
@@ -85,128 +81,99 @@ int main(int argc, char *argv[]) {
 
 /* handles files being read in */
 void readFile(char *file) {
-  // check if file is available
-  if (file != NULL) {
-    inputFile = fopen(file, "r");
-  } else {
-    inputFile = stdin;
-  }
-
-  int errnum;
+  // setup input source
+  inputFile = file != NULL ? fopen(file, "r") : stdin;
   if (inputFile == NULL) {
-    // file doesn't exist or cannot be opened for some reason - print error and
-    // immediately terminate execution
-    errnum = errno;
-    fprintf(stderr, "Error opening file: %s\n", strerror(errnum));
+    // file doesn't exist or cannot be opened for some reason
+    fprintf(stderr, "Error opening file: %s\n", strerror(errno));
     return;
   }
 
-  while (1) {
-    bool beginning =
-        false;  // variable to print number BEFORE any input is read
-    if (nflag || bflag) {
-      beginning = true;
-    }
+  // variable to print number BEFORE any input is read
+  bool beginning = nflag || bflag;
 
-    // resolve conflicting options if present
-    if (nflag && bflag) {
-      nflag = false;
-    } else if (tflag && vflag) {
-      vflag = false;
-    }
+  // resolve conflicting options if present
+  if (nflag && bflag) {
+    nflag = false;
+  } else if (tflag && vflag) {
+    vflag = false;
+  }
 
-    int line = 1;   // line number variable
-    int count = 1;  // variable that counts the amount of blank lines - used for
-                    // -s command
+  int lineNum = 1;
+  int blankLines = 1;
 
-    while (EOF != (input = fgetc(inputFile))) {
-      if (input == '\n') {
-        if (Eflag || eflag) {
-          if (sflag && count < 2) {
-            // to prevent $ being put too many times
-            fputc('$', stdout);
-          } else if (!sflag) {
-            // plain e/E operations
-            fputc('$', stdout);
-          }
-        }
-        if (bflag) {
-          // enable beginning line number print
-          beginning = true;
-        }
-        if (sflag) {
-          // increment count of new line
-          count++;
-        }
-      } else {
-        // set new line count to 0 as there is a character and not a new line to
-        // squeeze together
-        count = 0;
-        // print line number at beginning of file
-        if (beginning) {
-          printf("%6d\t", line);
-          line++;
-          beginning = false;
+  while (EOF != (input = fgetc(inputFile))) {
+    if (input == '\n') {
+      if (Eflag || eflag) {
+        if (sflag && blankLines < 2) {
+          // to prevent $ being put too many times
+          fputc('$', stdout);
+        } else if (!sflag) {
+          // plain e/E operations
+          fputc('$', stdout);
         }
       }
-      // to do while characters and not a series of blank lines
-      if (count <= 2) {
-        if (count > 1) {
-          // print a line number if sflag AND nflag enabled
-          if (sflag && nflag) {
-            printf("%6d\t", line);
-            line++;
-          }
-        }
+      if (bflag) {
+        // enable beginning line number print
+        beginning = true;
+      }
+      if (sflag) {
+        // increment count of new line
+        blankLines++;
+      }
+    } else {
+      // reset new line count as there is a character
+      blankLines = 0;
+      // print line number at beginning of file
+      if (beginning) {
+        printf("%6d\t", lineNum);
+        lineNum++;
+        beginning = false;
+      }
+    }
 
-        /*
-         Ascii actions based on the index of the character.
-            Before 33 is special characters
-            A = 65 TO Z = 90
-            a = 97 TO z = 122
-         */
-        //
-        if (vflag || tflag || eflag) {
-          if ((input < 33 && input >= 1) && (input != '\n')) {
-            if (tflag && input == '\t') {
-              input = input + 64;    /* forcing the special character to be a
-                                        printable ascii character by casting it 64
-                                        places to be in the uppercase range */
-              printf("^%c", input);  // print a hat then the assocated ascii
-                                     // character for the key pressed
-            } else if (input != '\t') {
-              input = input + 64;    /* forcing the special character to be a
-                                        printable ascii character by casting it 64
-                                        places to be in the uppercase range */
-              printf("^%c", input);  // print a hat then the assocated ascii
-                                     // character for the key pressed
-            } else {
-              printf("\t");  // if its a tab character tab it accross so it is
-                             // formatted as required
-            }
+    // to do while characters and not a series of blank lines
+    if (blankLines <= 2) {
+      if (blankLines > 1) {
+        // print a line number if sflag AND nflag enabled
+        if (sflag && nflag) {
+          printf("%6d\t", lineNum);
+          lineNum++;
+        }
+      }
+
+      if (vflag || tflag || eflag) {
+        if ((input < asciiControlCode && input >= 1) && (input != '\n')) {
+          if (input == '\t') {
+            // tab it accross so it is formatted as required
+            printf("\t");
           } else {
-            fputc(input, stdout);  // print normal ascii characters
+            // forcing the special character to be a printable ascii character
+            input += asciiSymbolEnd;
+            // print a hat to indicate it's escaped
+            printf("^%c", input);
           }
         } else {
-          fputc(input, stdout);  // Where ALL characters that are standardly
-                                 // read in are printed out!
-        }
-      }
-
-      // test for new line after the character has been printed
-      if (input == '\n') {
-        if (sflag && nflag) {
-          beginning = true;  // return to print number at beginning of line
-                             // before the next character
-        } else if (nflag) {
-          printf("%6d\t", line);  // print immediately the line number
-          line++;
+          // print regular ascii characters
+          fputc(input, stdout);
         }
       } else {
-        // do nothing as everything else will be caught
+        // print regular ascii characters
+        fputc(input, stdout);
       }
     }
-    fclose(inputFile);  // close the file
-    return;             // return to main
+
+    // test for new line after the character has been printed
+    if (input == '\n') {
+      if (sflag && nflag) {
+        beginning = true;  // return to print number at beginning of line
+        // before the next character
+      } else if (nflag) {
+        printf("%6d\t", lineNum);  // print immediately the line number
+        lineNum++;
+      }
+    }
   }
+  fclose(inputFile);
+  return;
 }
